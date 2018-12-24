12e715851ff46c5dee1184396477a23baa1128cf
MDerricott <mderricott@higherachievement.org>
2018-12-23 19:37:35 -0500

    push

---
+++ client/build/precache-manifest.d9e46ad2db7b85bcd1aa67f11c35020a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27055d04945a2f5385db",
+    "url": "/static/js/main.27055d04.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "27055d04945a2f5385db",
+    "url": "/static/css/main.7305cf4d.chunk.css"
+  },
+  {
+    "revision": "805420bd5a2b2642c142f48a90469134",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/app/client/build')));
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
